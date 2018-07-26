3cc6d8c6efe06947f4397caca0cde825b32a4f31
yanxinrui123 <642173450@qq.com>
2018-07-26 20:01:46 +0800

    Controller change
    Controller change

--- src/main/java/com/lanou/controller/UserController.java
+++ src/main/java/com/lanou/controller/UserController.java
@@ -1,7 +1,13 @@
 package com.lanou.controller;
 
+import org.springframework.stereotype.Controller;
+
 /**
  * Created by lanou on 2018/7/25.
  */
+@Controller
 public class UserController {
+    public  String login(){
+        return "login";
+    }
 }
